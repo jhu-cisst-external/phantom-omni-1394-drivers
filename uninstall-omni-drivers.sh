deb1b73eaf4aa73cd0e4bf7b1b3c1805c367c79d
Anton Deguet <anton.deguet@jhu.edu>
2024-04-08 17:56:06 -0400

    Update uninstall-omni-drivers.sh

--- uninstall-omni-drivers.sh
+++ uninstall-omni-drivers.sh
@@ -3,9 +3,13 @@
 # Anton Deguet anton.deguet@jhu.edu
 # 2017-12-07
 
+# create directory so post script doesn't fail
+mkdir -p /usr/share/3DTouch
+touch  /usr/share/3DTouch/dummy
+
 # uninstall deb packages
-apt-get remove --yes openhaptics-ae
-apt-get remove --yes phantomdevicedrivers
+apt-get remove --yes --show-progress openhaptics-ae
+apt-get remove --yes --show-progress phantomdevicedrivers
 
 # remove files copied manually from JUJU
 rm -f /usr/lib64/libPHANToM.so*
@@ -13,5 +17,5 @@ rm -f /usr/sbin/PHANToMConfiguration
 
 # remove OS configuration files
 rm -f /etc/ld.so.conf.d/openhaptics.conf
-rm -r /lib/udev/rules.d/50-phantom-firewire.rules
+rm -f /lib/udev/rules.d/50-phantom-firewire.rules
 udevadm control --reload-rules
